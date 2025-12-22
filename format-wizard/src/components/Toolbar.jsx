8dc2141b836c0eb46ecd382fb2e810637b9105fa
swapnil-railkar <swapnilrailkar01@gmail.com>
2025-12-22 17:48:42 +0530

    added toggle view

--- format-wizard/src/components/Toolbar.jsx
+++ format-wizard/src/components/Toolbar.jsx
@@ -8,13 +8,14 @@ import {
   TREE_VIEW,
   XML,
   YAML,
-  MINIFY
+  MINIFY,
 } from "../data/operation-constants";
 
-const NONE = 'none';
+const NONE = "none";
 export default function Toolbar({ selectOperation }) {
-  const[utilityOp, updateUtilityOp] = useState('');
-  const[formatOp, updateFormatOp] = useState('');
+  const [utilityOp, updateUtilityOp] = useState("");
+  const [formatOp, updateFormatOp] = useState("");
+  const [view, setView] = useState("json");
 
   function handleUtilityChange(event) {
     const utilityVal = event.target.value;
@@ -34,6 +35,27 @@ export default function Toolbar({ selectOperation }) {
     <section className="toolbar">
       <div className="wrapper">
         <img src="/app-logo-fox.png" height="20" width="20" />
+        <div className="view-toggle">
+          <input
+            type="radio"
+            id="jsonView"
+            name="view"
+            value="json"
+            checked={view === "json"}
+            onChange={() => setView("json")}
+          />
+          <label htmlFor="jsonView" className="text">JSON View</label>
+
+          <input
+            type="radio"
+            id="treeView"
+            name="view"
+            value="tree"
+            checked={view === "tree"}
+            onChange={() => setView("tree")}
+          />
+          <label htmlFor="treeView" className="text">Tree View</label>
+        </div>
       </div>
       <div className="wrapper">
         <select
